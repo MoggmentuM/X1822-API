d166bce23ce3dc1766be8a7be78d86ee2c0a5f35
Martin Forslund <martinforslund@hotmail.com>
2018-02-02 20:11:46 +0100

    first commit

---
+++ app/routes/note_routes.js
@@ -0,0 +1,43 @@
+var ObjectId = require('mongodb').ObjectId;
+
+module.exports = function(app, db){
+    
+    //C=Create
+    app.post('/notes', (req, res) => {
+
+        const myDB = db.db('notesdb112');
+        myDB.collection('notes');
+
+        const note = { text: req.body.body, title: req.body.title };
+        myDB.collection('notes').insert(note, (err, result)=>{
+            if (err){
+                res.send({'error': 'an error has occured'});
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+        console.log(req.body); //{ title: xxx, body: xxx}
+        //res.send('hello from post');
+    });
+    
+    //R=Read
+    app.get('/notes/:id', (req, res) => {
+
+        const myDB = db.db('notesdb112');
+        const id = req.params.id;
+        const details = {'_id' : new ObjectId(id)};
+        myDB.collection('notes').findOne(details, (err, item) => {
+            if (err){
+                res.send({'error': 'an error again...'});
+            } else {
+                res.send(item);
+            }
+        });
+        console.log('asking for a note');
+        //res.send('this should be a returned note');
+    });
+
+    //U=Update
+    
+    //D=Delete
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb://dbuser:t4db@ds121088.mlab.com:21088/notesdb112'
+};
